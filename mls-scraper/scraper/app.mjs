1cdbb9b26b2f6e8bc8d76a6f468bb223246396ef
Apalacios <Apalacios0020@gmail.com>
2023-10-20 09:20:33 -0700

    sam cli upgrade

---
+++ mls-scraper/scraper/app.mjs
@@ -0,0 +1,220 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+import puppeteer from "puppeteer-core";
+// import puppeteerExtra from "puppeteer-extra";
+// import stealthPlugin from "puppeteer-extra-plugin-stealth";
+import chromium from "@sparticuz/chromium";
+
+const removeSymbols = (string) => {
+  return string.replace(/[!-\/:-@[-`{-~]/g, "");
+};
+//
+//
+//
+async function redfinAddressScrape(mlsOrAddress) {
+  chromium.setHeadlessMode = true;
+  try {
+    // puppeteerExtra.use(stealthPlugin());
+
+    // const browser = await puppeteerExtra.launch({
+    //   executablePath:
+    //     "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    //   headless: "new",
+    //   defaultViewport: null,
+    // });
+
+    // const browser = await puppeteerExtra.launch({
+    //   args: chromium.args,
+    //   executablePath: await chromium.executablePath(),
+    //   headless: chromium.headless,
+    //   defaultViewport: chromium.defaultViewport,
+    //   ignoreHTTPSErrors: true,
+    // });
+    console.log(chromium.args);
+
+    const browser = await puppeteer.launch({
+      args: chromium.args.concat([
+        "--disable-features=site-per-process",
+        "--hide-scrollbars",
+        "--disable-web-security",
+      ]),
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+    console.log("im her2!");
+
+    const page = await browser.newPage();
+    await page.setViewport({ width: 800, height: 600 });
+    page.setDefaultNavigationTimeout(0);
+    console.log("im her3!");
+
+    await page.goto("https://www.redfin.com/");
+    // await page.screenshot({
+    //   path: "screenshot.jpg",
+    //   fullPage: true,
+    // });
+    await page.waitForTimeout(5000);
+    // await page.screenshot({ path: "home.png", fullPage: true });
+    console.log("launched");
+
+    console.log(mlsOrAddress);
+    await page.type("#search-box-input", mlsOrAddress, { delay: 0 });
+    console.log("yung money");
+    // await page.screenshot({
+    //   path: "/tmp/screenshot2.jpg",
+    //   fullPage: true,
+    // });
+    // await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+    // await page.waitForSelector(
+    //   ".inline-block.SearchButton.clickable.float-right"
+    // );
+
+    // CLICKER
+    await page.waitForTimeout(5000);
+    const clicker = await page.$(
+      ".inline-block.SearchButton.clickable.float-right"
+    );
+    try {
+      await page.evaluate((el) => {
+        return el.click();
+      }, clicker);
+      await page.waitFor(1500);
+    } catch (e) {
+      console.log("click error", e);
+    }
+
+    // await page.click(".inline-block.SearchButton.clickable.float-right");
+    await page.waitForTimeout(5000);
+    // await page.screenshot({
+    //   path: "/tmp/screenshot3.jpg",
+    //   fullPage: true,
+    // });
+    console.log("waiting");
+    // await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+
+    await page.waitForSelector(".statsValue", { timeout: 120000 });
+    // await page.screenshot({ path: "stealth.png", fullPage: true });
+    const infoX = await page.$$("[class='statsValue']");
+    const [price, bedrooms, bathrooms, sqft] = await Promise.all([
+      (await infoX[0].getProperty("textContent")).jsonValue(),
+      (await infoX[1].getProperty("textContent")).jsonValue(),
+      (await infoX[2].getProperty("textContent")).jsonValue(),
+      (await infoX[3].getProperty("textContent")).jsonValue(),
+    ]);
+    console.log(price);
+    console.log(bathrooms);
+    console.log(bedrooms);
+    console.log(sqft);
+
+    const addressX = await page.$("[class='street-address']");
+    const address = await (
+      await addressX?.getProperty("textContent")
+    )?.jsonValue();
+    console.log("address: " + address);
+
+    const cityStateZipX = await page.$("[class='dp-subtext bp-cityStateZip']");
+    const cityStateZip = await (
+      await cityStateZipX?.getProperty("textContent")
+    )?.jsonValue();
+    console.log(cityStateZip);
+    const city = cityStateZip?.split(",")[0];
+    const state = cityStateZip?.split(",")[1].split(" ")[1];
+    const zip = cityStateZip?.split(",")[1].split(" ")[2];
+
+    const lotSizeX = await page.$$("[class='table-value']");
+    const lotSize = await (
+      await lotSizeX[4].getProperty("textContent")
+    ).jsonValue();
+    console.log(lotSize);
+
+    const mlsX = await page.$("[class='ListingSource--mlsId']");
+    const mls = await (await mlsX?.getProperty("textContent"))?.jsonValue();
+    console.log(mls);
+
+    const previewX = await page.$(".landscape");
+    const previewSrc = (await previewX?.getProperty("src"))?.jsonValue();
+    const promise = new Promise((resolve) => {
+      resolve(previewSrc);
+    });
+    const preview = await Promise.resolve(promise);
+    console.log(preview);
+
+    const pages = await browser.pages();
+    await Promise.all(pages.map(async (page) => page.close()));
+    console.log("closed");
+
+    const res = {
+      data: {
+        preview: preview,
+        mls: removeSymbols(mls),
+        price: removeSymbols(price),
+        bedrooms: bedrooms,
+        bathrooms: bathrooms,
+        sqft: removeSymbols(sqft),
+        lot_Size: lotSize,
+        city: city,
+        state: state,
+        zip: zip,
+      },
+    };
+
+    const mlsData = JSON.stringify(res);
+    console.log(mlsData);
+
+    return mlsData;
+  } catch (error) {
+    console.log("error at the scrape", error.message);
+  }
+}
+//
+//
+//
+// export const handler = async (event, context) => {
+//     try {
+//       console.log("started");
+//       const body = JSON.parse(event.body);
+//       const { mlsOrAddress } = body;
+
+//       const data = await redfinAddressScrape(mlsOrAddress);
+//       console.log("Handler Data:", data);
+
+//       return {
+//         statusCode: 200,
+//         body: JSON.stringify(data),
+//       };
+//     } catch (error) {
+//       console.log("error with index.js", error.message);
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({
+//           message: error.message,
+//         }),
+//       };
+//     }
+//   };
+
+export const lambdaHandler = async (event, context) => {
+  try {
+    return {
+      statusCode: 200,
+      body: await redfinAddressScrape(event.body),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
+};
